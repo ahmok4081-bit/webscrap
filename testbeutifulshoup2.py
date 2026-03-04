0ce2557dfea080bd2e21a98e1469751124505ff0
Pun viraksakada <ahmok4081@gmail.com>
2026-03-04 21:29:30 +0700

    7:17 tech with tim how to use tag

--- testbeutifulshoup2.py
+++ testbeutifulshoup2.py
@@ -2,7 +2,8 @@ from bs4 import BeautifulSoup
 
 with open("index.html","r") as f:
     doc = BeautifulSoup(f, "html.parser")
-print(doc.prettify())
+#print(doc.prettify())
 
 tag = doc.title
+tag.string = "hello"
 print(tag)
