a1449dbbf50bc2663ce0bcba891bed5f3ba3bdbf
Pun viraksakada <ahmok4081@gmail.com>
2026-03-04 21:56:23 +0700

    11:30 get the information from url

--- testbeutifulshoup2.py
+++ testbeutifulshoup2.py
@@ -1,8 +1,5 @@
 from bs4 import BeautifulSoup
-
-with open("index.html","r") as f:
-    doc = BeautifulSoup(f, "html.parser")
-#print(doc.prettify())
-
-tag = doc.find_all("p")[0]
-print(tag.find_all("b"))
+import requests
+url = "https://results.first.global/"
+result = requests.get(url)
+print(result.text)
