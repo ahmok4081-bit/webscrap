aafaa9c0186ddad016872197760c17c04a1c5d4c
Pun viraksakada <ahmok4081@gmail.com>
2026-03-04 23:41:16 +0700

    from 13:55 to end

--- testbeutifulshoup2.py
+++ testbeutifulshoup2.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
-url = "https://results.first.global/"
+url = "https://www.newegg.ca/intel-core-i9-12th-gen-core-i9-12900k-alder-lake-lga-1700-desktop-cpu-processor/p/N82E16819118339?Item=N82E16819118339"
 result = requests.get(url)
-print(result.text)
+doc = BeautifulSoup(result.text, "html.parser")
+prices = doc.find_all (text = "$")
+parent = prices[0].parent
+strong = parent.find('strong')
+print(strong.string)
