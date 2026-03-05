2e320a0790397d03927e499eab2361c3b39f6aaa
Pun viraksakada <ahmok4081@gmail.com>
2026-03-05 10:01:23 +0700

    V2 find multiple  tags

--- testbeutifulshoup2.py
+++ testbeutifulshoup2.py
@@ -1,26 +1,5 @@
 from bs4 import BeautifulSoup
-
-html = """
-<html>
-  <body>
-    <p class="msg">Hello</p>
-    <p class="msg">World</p>
-  </body>
-</html>
-"""
-
-soup = BeautifulSoup(html, "html.parser")
-
-# find() → first match only
-first_p = soup.find("p", class_="msg")
-print(first_p.text)   # Output: Hello
-
-# find_all() → all matches
-all_p = soup.find_all("p", class_="msg")
-for p in all_p:
-    print(p.text)     # Output: Hello, World
-#Use find() when you expect or only care about one element.
-
-#Use find_all() when you want to collect and iterate over multiple elements.
-
-#Think of find() as "give me the first match" and find_all() as "give me everything that matches."
+with open("index.html","r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+tag = doc.find_all(["P","div","li"])
+print(tag)
