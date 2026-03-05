26483953f7f6bdc9d383ee4ff7eecbfa6d9d308a
Pun viraksakada <ahmok4081@gmail.com>
2026-03-05 10:14:19 +0700

    looking insde tag

--- testbeutifulshoup2.py
+++ testbeutifulshoup2.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
 with open("index.html","r") as f:
     doc = BeautifulSoup(f, "html.parser")
-tag = doc.find_all(["P","div","li"])
+tag = doc.find_all(class_= "btn-item")
 print(tag)
