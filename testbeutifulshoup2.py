ee21edf2ad72ac9808445039701063888ba3667a
Pun viraksakada <ahmok4081@gmail.com>
2026-03-09 22:17:32 +0700

    from book to scrape

--- testbeutifulshoup2.py
+++ testbeutifulshoup2.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
+import requests
 with open("index.html","r") as f:
     doc = BeautifulSoup(f, "html.parser")
-tag = doc.find_all(class_= "btn-item")
+tag = doc.find_all(text= re.compile("\$.*"))
 print(tag)
