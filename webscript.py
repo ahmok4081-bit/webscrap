5809dd688fd6db76e1efae3ea8007a7ad654f7d7
Pun viraksakada <ahmok4081@gmail.com>
2026-03-30 23:18:14 +0700

    scrap with mutiple page

--- webscript.py
+++ webscript.py
@@ -1,7 +1,24 @@
+import requests
 from bs4 import BeautifulSoup
+import csv
 
-with open("index.html", "r") as f:
-    doc = BeautifulSoup(f, "html.parser")
-tag = doc.find_all(["p", "div", "li"])
-print(tag)
-print(tag)
+with open("books.csv", "w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Price", "Rating", "Link"])
+
+    for page in range(1, 51):   # change to 51 later
+        url = f"https://books.toscrape.com/catalogue/page-{page}.html"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        all_books = soup.find_all("article", class_="product_pod")
+
+        for book in all_books:
+            title = book.h3.a["title"]
+            price = book.find("p", class_="price_color").text
+            rating = book.find("p", class_="star-rating")["class"][1]
+            link = book.h3.a["href"]
+
+            writer.writerow([title, price, rating, link])
+
+print("Done scraping!")
